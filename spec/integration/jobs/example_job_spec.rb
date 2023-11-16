faab2f9baacdc7d35bb05f49ae4ea80af30dd854
Mario G <mariojgintili@gmail.com>
2023-11-16 10:03:01 +0000

    add test

--- spec/integration/jobs/example_job_spec.rb
+++ spec/integration/jobs/example_job_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe ExampleJob, type: :job do
   describe "karafka active job integration" do
     it "queueing a job to be executed later works" do
+      expect(true).to eq(true)
     end
     describe "retries" do
       it "a failed job will retry in 3 seconds" do
